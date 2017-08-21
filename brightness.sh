16e6bf40b9525d56956a8ae495090049ad9a28d3
Alessandro <masterchieff02@gmail.com>
2017-08-22 01:25:25 +0200

    Restricted search of monitor brightness
    $INFO store only the first 10 lines of "xrandr --current --verbose". Since we aren't sure that the first monitor that shows up is always the laptop's one, this give us the security we want.
    After that check it extract the current brightness.

--- brightness.sh
+++ brightness.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-CURRBRIGHT=$(xrandr --current --verbose | grep -m 1 'Brightness:' | cut -f2- -d:)
+#create string with info -> only the first 10 lines
+INFO=$(xrandr --current --verbose | head -n 10)
+
+#make sure that the captured lines are relative of the monitor's laptop
+if [grep -q '$2' $INFO] && [grep -q "connected" $INFO] && [grep -q 'Brightness:' $INFO]
+
+#give the current brightness
+CURRBRIGHT=$(grep -m 1 'Brightness:' $INFO | cut -f2- -d:)
+
+then 
+#comparision in order to determinate if up or down the brightness
 if [ "$1" = "+" ] && [ $(echo "$CURRBRIGHT < 1" | bc) -eq 1 ] 
 then
 xrandr --output $2 --brightness $(echo "$CURRBRIGHT + 0.1" | bc)
